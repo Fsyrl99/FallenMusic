a68a8aa68ac36aa540879980424405b97920046a
Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>
2024-07-07 09:44:16 +0700

    Update config.py
    Signed-off-by: Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>

--- config.py
+++ config.py
@@ -25,3 +25,31 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+
+EXTRA_PLUGINS = getenv(
+    "EXTRA_PLUGINS",
+    "True",
+)
+
+# Fill True if you want to load extra plugins
+
+
+EXTRA_PLUGINS_REPO = getenv(
+    "EXTRA_PLUGINS_REPO",
+    "https://github.com/Fsyrl99/ferdimusik",
+)
+# Fill here the external plugins repo where plugins that you want to load
+
+SUPPORT_CHANNEL = getenv(
+    "SUPPORT_CHANNEL", "https://t.me/Nenen_degrees"
+
+  SUPPORT_GROUP = getenv(
+    "SUPPORT_GROUP", "galeribv"
+
+# Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
+
+# Time after which you're assistant account will leave chats automatically.
+AUTO_LEAVE_ASSISTANT_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", "50000")
+)  # Remember to give value in Seconds
