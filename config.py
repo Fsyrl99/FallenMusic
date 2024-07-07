552428059522ead18d2cc5ac9623f86047255848
Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>
2024-07-07 10:45:58 +0700

    Update config.py
    Signed-off-by: Fsyrl99 <118256932+Fsyrl99@users.noreply.github.com>

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Fsyrl99/ferdimusik",
+    "https://github.com/Fsyrl99/ferdimusik/sma-master",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
